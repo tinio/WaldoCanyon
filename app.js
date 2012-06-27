7f4bdf0a546c2e74b713d6f36d2838cdf04d5184
Robbie Trencheny <me@robbiet.us>
2012-06-26 20:38:30 -0600

    whoops port

--- app.js
+++ app.js
@@ -64,6 +64,8 @@ app.get('/', function(req,res){
 	res.render('index',{title:'Index'})
 });
 
-app.listen(3000, function(){
-	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
