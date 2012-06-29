10812ad5b2ffd1ab2b43ff3cef576b23de9b153a
Robbie Trencheny <me@robbiet.us>
2012-06-28 21:25:40 -0400

    More fixes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
 
 var express = require('express')
 	, twitter = require('ntwitter')
-	, mongoose = require('mongoose');
+	, mongoose = require('mongoose')
+	, request = require('request');
 
 var app = module.exports = express.createServer();
 
@@ -39,34 +40,36 @@ io.configure(function () {
 
 // Twitter
 
-//WaldoCanyon
-var twit = new twitter({
-	consumer_key: 'Ktz7HRoA3PGr6ARNzIXcag',
-	consumer_secret: 'ZUsnNEhQ3vXjx2ZjLtuUYUyiCJtmYkuG3wNtRC2M',
-	access_token_key: '46713-aqtHStll8KiHOKjQSZQdqdMWXitypZkThBA47o96J0',
-	access_token_secret: 'iM7jvgIA8s5vDUjwCki1G31sJaGQ6746LgHHQtbbE'
-});
-
-//WaldoCanyonTest
-/*var twit = new twitter({
-	consumer_key: 'WAJWosqcXWcYAhL3pFd9Q',
-	consumer_secret: '27JJV5ml8ukMkcBrVRB9xTG22YzpSJAQn4FrydnGfXY',
-	access_token_key: '46713-ezpnVWoQtVfuBHRMjJImPHvQMXIq4uXIIVX9MFQGDY',
-	access_token_secret: 'cfAB65cK4tMyLeKmir4bPbThmNvEqJ4YEJ8JFco88'
-});*/
+if(process.env.NODE_ENV == "production") {
+	//WaldoCanyon
+	var twit = new twitter({
+		consumer_key: 'Ktz7HRoA3PGr6ARNzIXcag',
+		consumer_secret: 'ZUsnNEhQ3vXjx2ZjLtuUYUyiCJtmYkuG3wNtRC2M',
+		access_token_key: '46713-aqtHStll8KiHOKjQSZQdqdMWXitypZkThBA47o96J0',
+		access_token_secret: 'iM7jvgIA8s5vDUjwCki1G31sJaGQ6746LgHHQtbbE'
+	});
+} else {
+	//WaldoCanyonTest
+	var twit = new twitter({
+		consumer_key: 'WAJWosqcXWcYAhL3pFd9Q',
+		consumer_secret: '27JJV5ml8ukMkcBrVRB9xTG22YzpSJAQn4FrydnGfXY',
+		access_token_key: '46713-ezpnVWoQtVfuBHRMjJImPHvQMXIq4uXIIVX9MFQGDY',
+		access_token_secret: 'cfAB65cK4tMyLeKmir4bPbThmNvEqJ4YEJ8JFco88'
+	});
+}
 
 twit.stream('statuses/filter', {'track':'#WaldoCanyonFire,#WaldoFire,#waldocanyon,Colorado Springs'}, function(stream) {
 	stream.on('data', function (data) {
 		io.sockets.emit('tweet',data);
 	});
 	stream.on('error', function(err){
-		console.log(err)
+			console.error(err)
 	});
 	stream.on('end', function (response) {
-					console.log(response);
+			console.error(response);
 	});
 	stream.on('destroy', function (response) {
-		console.log(response);
+		console.error(response);
 	});
 });
 
@@ -86,6 +89,17 @@ app.get('/about', function(req,res){
 	res.render('about',{title:'About'})
 });
 
+app.post('/instagram_receiver', function(req,res){
+	console.log(req.body);
+	console.log('fetching: http://api.instagr.am/tags/'+req.body.tag+'/recent')
+	request('http://api.instagr.am/tags/'+req.body.tag+'/recent', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body) // Print the google web page.
+	    io.sockets.emit('instagram',body);
+	  }
+	});
+	res.end('ok');
+});
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
